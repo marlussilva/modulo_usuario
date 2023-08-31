6f4bdeacbdcae4c1b2547909879ae28a41d3058e
riverfix <74383761+riverfix@users.noreply.github.com>
2023-08-31 10:49:28 -0300

    camera renderizando

--- lib/screen/camera/mac/camera_mac_screen.dart
+++ lib/screen/camera/mac/camera_mac_screen.dart
@@ -19,38 +19,37 @@ class _CameraMacScreenState extends State<CameraMacScreen> {
     super.initState();
     // Supondo que você tenha um método para inicializar a câmera
     cameraStore.listVideoDevices();
-
-    cameraStore.listAudioDevices();
-    cameraStore.startRecording();
-    cameraStore.onCameraButtonTap();
-    cameraStore.changeCameraMode();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Camera Page')),
+      //  appBar: AppBar(title: Text('Camera Page')),
       body: Observer(
         builder: (_) {
           if (cameraStore.selectedVideoDevice == null) {
             return Center(child: CircularProgressIndicator());
           }
 
-          return CameraMacOSView(
-            cameraMode: cameraStore.cameraMode,
-            deviceId: cameraStore.selectedVideoDevice,
-            //audioDeviceId: cameraStore
-            //   .selectedAudioDevice, // Isto é uma suposição; se o seu aplicativo não suportar áudio, ajuste conforme necessário.
-            enableAudio: cameraStore.enableAudio,
-            onCameraInizialized: (controller) {
-              cameraStore.macOSController = controller;
-            },
-            onCameraLoading: (error) {
-              return Center(child: Text("Erro ao carregar a câmera: $error"));
-            },
-            onCameraDestroyed: () {
-              return Center(child: Text("Câmera destruída."));
-            },
+          return Container(
+            width: 200,
+            height: 200,
+            child: CameraMacOSView(
+              cameraMode: cameraStore.cameraMode,
+              deviceId: cameraStore.selectedVideoDevice,
+              //audioDeviceId: cameraStore
+              //   .selectedAudioDevice, // Isto é uma suposição; se o seu aplicativo não suportar áudio, ajuste conforme necessário.
+              enableAudio: cameraStore.enableAudio,
+              onCameraInizialized: (controller) {
+                cameraStore.macOSController = controller;
+              },
+              onCameraLoading: (error) {
+                return Center(child: Text("Erro ao carregar a câmera: $error"));
+              },
+              onCameraDestroyed: () {
+                return Center(child: Text("Câmera destruída."));
+              },
+            ),
           );
         },
       ),
